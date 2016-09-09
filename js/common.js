005d2e0040db3dda6fac17e376628abf32f0026a
Control-kun <hvostovp@gmail.com>
2016-09-10 01:11:15 +0300

    Добавил стартовый пакет файлов
    Верстай как тебе удобно, плюс мы вчера еще обсуждали

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
