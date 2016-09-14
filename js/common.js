d67a1f2f41f7795d4aa84640891929dc770ac5fd
Pavel Khvostov <hvostovp@gmail.com>
2016-09-15 01:00:30 +0300

    Начал слайдер делать

--- js/common.js
+++ js/common.js
@@ -11,3 +11,30 @@ $(function() {
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
 });
+$( document ).ready(function() {
+	$('.production-slider').slick({
+		centerMode: true,
+		centerPadding: '60px',
+		slidesToShow: 1,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '72px',
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 360,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+});
